713c7af9f163900eaa44b09ee22e68185bae38ea
Binh Tran <tranhuubinh223@gmail.com>
2022-08-30 11:19:02 +0700

    Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :find_user, only: %i(show edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
 
-    flash[:danger] = t(".user_not_found")
-    redirect_to root_path
+  def index
+    @pagy, @users = pagy User.order(Settings.user.index_user_order),
+                         items: Settings.user.user_per_page
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -23,10 +27,61 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t(".user_updated")
+      redirect_to @user
+    else
+      flash.now[:danger] = t(".update_failed")
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t(".user_deleted")
+    else
+      flash[:danger] = t(".delete_failed")
+    end
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t(".login_required")
+    redirect_to login_url
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t(".user_not_found")
+    redirect_to root_path
+  end
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:danger] = t(".wrong_user")
+    redirect_to root_url
+  end
+
+  def admin_user
+    return if current_user.admin?
+
+    flash[:danger] = t(".admin_required")
+    redirect_to root_url
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_24_020435) do
-
+ActiveRecord::Schema.define(version: 2022_08_28_145153) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,7 +18,7 @@ ActiveRecord::Schema.define(version: 2022_08_24_020435) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
